551b89373339041a37220d73777e9f015734a7ff
bipni <bipumirja@gmail.com>
2024-01-30 00:01:42 +0600

    page scrape added

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
-HEADER_NAMES = ['post_id', 'post_url', 'post_text', 'reaction_count',
-                'profile_id', 'profile_name', 'profile_url', 'post_time', 'comments',
-                'group_id', 'group_name', 'group_url', 'community', 'group_about']
+GROUP_HEADER_NAMES = ['post_id', 'post_url', 'post_text', 'reaction_count',
+                      'profile_id', 'profile_name', 'profile_url', 'post_time', 'comments',
+                      'group_id', 'group_name', 'group_url', 'community', 'group_about']
+
+PAGE_HEADER_NAMES = ['post_id', 'post_url', 'post_text', 'reaction_count',
+                     'profile_id', 'profile_name', 'profile_url', 'post_time', 'comments',
+                     'page_id', 'page_name', 'page_url', 'community', 'page_about']
--- page_scraper.py
+++ page_scraper.py
@@ -1,17 +1,17 @@
 import csv
 from os.path import exists
 
-from fb_scraper import get_group_posts_by_group_id
+from fb_scraper import get_page_posts_by_page_id
 
-from constants import GROUP_HEADER_NAMES
+from constants import PAGE_HEADER_NAMES
 
-GROUP_ID = '140473731634479'
+PAGE_ID = 'earkidotcom'
 COOKIES_NAME = ['keye.txt']
 COMMUNITY = 'Information'
-GROUP_URL = f'https://mbasic.facebook.com/groups/{GROUP_ID}'
-GROUP_NAME = 'HM Tour & Travels (ট্রাভেলিং গ্রুপ)'
-GROUP_ABOUT = 'Travel Group'
-FILE_NAME = f'{GROUP_ID}.csv'
+PAGE_URL = f'https://mbasic.facebook.com/{PAGE_ID}'
+PAGE_NAME = 'Earki'
+PAGE_ABOUT = 'Comedy'
+FILE_NAME = f'{PAGE_ID}.csv'
 START_URL = None
 
 page = 1
@@ -21,13 +21,13 @@ next_url = None
 if not exists(f'files/{FILE_NAME}'):
     with open(f'files/{FILE_NAME}', 'w', encoding='utf-8', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(GROUP_HEADER_NAMES)
+        csv_writer.writerow(PAGE_HEADER_NAMES)
 
 while True:
     if not next_url:
-        data = get_group_posts_by_group_id(group_id=GROUP_ID, cookies=f'cookies/{COOKIES_NAME[0]}', start_url=START_URL)
+        data = get_page_posts_by_page_id(page_id=PAGE_ID, cookies=f'cookies/{COOKIES_NAME[0]}', start_url=START_URL)
     else:
-        data = get_group_posts_by_group_id(group_id=GROUP_ID, cookies=f'cookies/{COOKIES_NAME[0]}', start_url=next_url)
+        data = get_page_posts_by_page_id(page_id=PAGE_ID, cookies=f'cookies/{COOKIES_NAME[0]}', start_url=next_url)
 
     print(f'Next URL: {data["next_url"]}')
     next_url = data['next_url']
@@ -44,23 +44,23 @@ while True:
     print(f'Cookie Using: {COOKIES_NAME[cookie_index]}')
 
     if data:
-        group_posts = data['group_posts']
+        page_posts = data['page_posts']
 
-        for group_post in group_posts:
-            if group_post['post_id'] not in post_ids:
+        for page_post in page_posts:
+            if page_post['post_id'] not in post_ids:
                 copy_dict = {}
 
-                for header in GROUP_HEADER_NAMES:
-                    if header in list(group_post.keys()):
-                        copy_dict[header] = group_post[header]
+                for header in PAGE_HEADER_NAMES:
+                    if header in list(page_post.keys()):
+                        copy_dict[header] = page_post[header]
                     else:
                         copy_dict[header] = None
 
                 copy_dict['community'] = COMMUNITY
-                copy_dict['group_id'] = GROUP_ID
-                copy_dict['group_url'] = GROUP_URL
-                copy_dict['group_name'] = GROUP_NAME
-                copy_dict['group_about'] = GROUP_NAME
+                copy_dict['page_id'] = PAGE_ID
+                copy_dict['page_url'] = PAGE_URL
+                copy_dict['page_name'] = PAGE_NAME
+                copy_dict['page_about'] = PAGE_NAME
 
                 if exists(f'files/{FILE_NAME}'):
                     with open(f'files/{FILE_NAME}', 'a', newline='', encoding='utf-8') as f:
@@ -72,4 +72,4 @@ while True:
                         writer.writeheader()
                         writer.writerow(copy_dict)
 
-                post_ids.append(group_post['post_id'])
+                post_ids.append(page_post['post_id'])
