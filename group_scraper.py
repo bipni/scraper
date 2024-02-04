f8a5a96e34f4ccd33fd6e24e7171933b2153abd8
bipni <bipumirja@gmail.com>
2024-02-04 16:15:39 +0600

    cookie issue fixed

--- group_scraper.py
+++ group_scraper.py
@@ -37,6 +37,9 @@ while True:
     if not next_url:
         print('This group might not have any posts available')
         print(f'or {cookie_files} cookie is invalid')
+        cookie_index = page % len(COOKIES_NAME)
+        cookie_files = f'cookies/{COOKIES_NAME[cookie_index]}'
+        print(f'Cookie Using: {COOKIES_NAME[cookie_index]}')
         continue
 
     print(next_url)
